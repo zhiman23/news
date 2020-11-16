8e7f196a50803e15b3b3b68b0be37b9900e98b1f
yxy <2519675951@qq.com>
2020-11-16 14:51:35 +0800

    add a login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import Login from '../views/login.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,6 +12,7 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  { path: '/login', component: Login },
   {
     path: '/about',
     name: 'About',
